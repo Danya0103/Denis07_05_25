a73306ba090daa9c27f44fdbe82c69c6564b372a
Danya0103 <user@WIN-LRBB7CDB9IA>
2025-05-09 17:46:01 +0300

    RomanToInteger

--- Denis07_05_25/Program.cs
+++ Denis07_05_25/Program.cs
@@ -2,8 +2,33 @@
 
 class Program {
 
-    static void Main(string[] args){
+    static void Main(string[] args) {
+
+        Console.WriteLine(RomanToInteger("M"));
 
     }
 
+    static int RomanToInteger(string number) {
+
+        switch (number) {
+
+            case "I":
+                return 1;
+            case "V":
+                return 5;
+            case "X":
+                return 10;
+            case "L":
+                return 50;
+            case "C":
+                return 100;
+            case "D":
+                return 500;
+            case "M":
+                return 1000;
+            default:
+                return 0;
+
+        }
+    }
 }
