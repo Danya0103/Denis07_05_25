4ab180e3eb17eee1583a66f5b740896f2e932612
Danya0103 <user@WIN-LRBB7CDB9IA>
2025-05-10 13:39:11 +0300

    Readr without -

--- Denis07_05_25/Program.cs
+++ Denis07_05_25/Program.cs
@@ -1,30 +1,41 @@
 ﻿using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program {
 
     static void Main(string[] args) {
 
-        Console.WriteLine(RomanToInteger("M"));
+        string seven = "XIX";
+        int result = 0;
 
+        for (int i = 0; i < seven.Length; i++)
+        {
+            result += RomanToInteger(seven[i]);
+            
+        }
+        // 0, 5, 5 + 1 = 6, 6 + 1 = 7.
+        Console.WriteLine(result);
     }
 
-    static int RomanToInteger(string number) {
+
+
+    static int RomanToInteger(char number) {
 
         switch (number) {
 
-            case "I":
+            case 'I':
                 return 1;
-            case "V":
+            case 'V':
                 return 5;
-            case "X":
+            case 'X':
                 return 10;
-            case "L":
+            case 'L':
                 return 50;
-            case "C":
+            case 'C':
                 return 100;
-            case "D":
+            case 'D':
                 return 500;
-            case "M":
+            case 'M':
                 return 1000;
             default:
                 return 0;
